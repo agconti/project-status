81c2bcb534ac7aa31ce54c58a538b9ac90d9b2e2
agconti <agc11d@gmail.com>
2016-01-20 16:56:05 -0500

    feat(index): basic implementation to get gh issues

---
+++ index.js
@@ -0,0 +1,16 @@
+const Rx = require('rx')
+const request = Rx.Observable.fromNodeCallback(require('request'))
+
+const oauthToken = process.argv[2]
+const owner = process.argv[3]
+const repository = process.argv[4]
+const url = `https://api.github.com/repos/${owner}/${repository}/issues`
+const headers = {
+	'Authorization': `token ${oauthToken}`
+	, 'User-Agent': owner
+}
+
+
+request({url, headers})
+	.subscribe( result => console.log(result)
+						, err => console.error(err))
