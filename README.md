61496dbb68ca3fe370d8ebc335fcb2ff95d85b41
Conti <agc11d@gmail.com>
2017-03-13 14:01:39 -0400

    refactor: convert to open prs

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
-# github-issues-to-markdown
-A simple utility for easily extracting github issues
+# my-open-prs
+![](https://media0.giphy.com/media/l2YWogPxLTLNSC9Dq/giphy.gif)
+What PRs do I have open?!?!
+
+This is a simple utility that quickly gets and formats your outstanding pull requests. PRs are filtered by the `Ready for code review label` and are formatted nicely so you can alert your team to take a look:
+
+```
+- feat: super cool feature 😱  ( Opened 13 days ago ) (https://github.com/your-org/project/pull/1) L ⌚, Ready for code review ⚡
+- feat: omg moar features ( Opened 48 days ago ) (https://github.com/your-org/project/pull/2) Ready for code review ⚡, S ❤️
+- Fix: oh no. squashed a bug! ( Opened 4 days ago ) (https://github.com/your-org/project/pull/3) L ⌚, Ready for code review ⚡, wooops!
+```
+
+# Install
+Clone this repository:
+```
+git clone git@github.com:agconti/my-open-prs.git
+```
+
+Get a [personal access toke](https://github.com/settings/tokens) and add it a file named `key.txt`:
+```
+echo "your-token" > key.txt
+```
+
+Update the start command to include your user.
+```
+"start": "node index.js $(cat key.txt) giphy web-app :your-user"
+```
 
 # Usage
 
-node index.js :token :owner :repository
+node index.js :token :owner :repository :user
 
 Ex:
 ```bash
-node index.js 3d2dsafe3f942b5f6b44c114121 agconti github-issues-to-markdown
+node index.js 3d2dsafe3f942b5f6b44c114121 giphy web-app agconti
+```
+
+or:
+```
+npm start
 ```
--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const Rx = require('rx')
 const request = Rx.Observable.fromNodeCallback(require('request'))
 
-const oauthToken = process.argv[2]
-const owner = process.argv[3]
-const repository = process.argv[4]
-const url = `https://api.github.com/repos/${owner}/${repository}/issues`
+const [nodePath, filePath, oauthToken, owner, repository, user] = process.argv
+const url = `https://api.github.com/repos/${owner}/${repository}/issues?creator=${user}`
 const headers = {
 		'Authorization': `token ${oauthToken}`
-	, 'User-Agent': owner
+	, 'User-Agent': `my-open-prs-app`
 }
 
-const makeTodoItem = issue => {
-	const { title, number } = issue
-	const issueNumber = issue.url.split('/').slice(-1)
-	const url = `https://github.com/${owner}/${repository}/issues/${issueNumber}`
+const readyForCodeReview = ({labels}) => {
+	if (!labels) {
+		return false
+	}
 
-	return `- [ ] ${title} [#${number}](${url})`
+	return labels.some(label => label.name === 'Ready for code review ⚡')
+}
+
+const makeReviewItem = issue => {
+	const { title, number, created_at, html_url, labels } = issue
+	const now = new Date()
+	const opened = new Date(created_at)
+	const daysSinceOpened = Math.round(Math.abs(now - opened) / 8.64e7)
+	const status = labels.map(label => label.name).join(", ")
+
+	return `- ${title} ( Opened ${daysSinceOpened} days ago ) (${html_url}) ${status}`
 }
 
 request({url, headers})
 	.map(res => JSON.parse(res[0].body))
 	.flatMap(issues => issues.map(issue => issue))
-	.filter(issue => issue.state == 'open')
-	.map(makeTodoItem)
+	.filter(issue => issue.hasOwnProperty('pull_request'))
+	.filter(readyForCodeReview)
+	.map(makeReviewItem)
 	.subscribe( result => console.log(result)
 						, err => console.error(err))
